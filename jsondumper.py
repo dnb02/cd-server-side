70f74881875a92fb62c5b55e13e1e4a463ba8b8e
Dev <dnb.ranjikaa@gmail.com>
2021-06-28 21:40:09 +0530

    dumping into reddit's r/jsondump

--- jsondumper.py
+++ jsondumper.py
@@ -5,6 +5,8 @@ import os
 clientSecret = os.environ.get('REDDIT_SECRET_KEY')
 password = os.environ.get('BOTT_PASSWORD')
 reddit = praw.Reddit(client_id='WvfB2YOqYcUlMg',client_secret=clientSecret,username='jsonbot',password=password,user_agent='prawtryv1')
-selftext = str({'k':'l','m':'j'})
-title = "PRAW documentation"
+with open('dump.json') as inp:
+	dumpValue=json.load(inp)
+selftext = str(dumpValue)
+title = "-json-dump-"
 reddit.subreddit("jsondump").submit(title, selftext=selftext)
